bc37410425b7e1b72911080d7dc8dab7c72cacd4
Sharath <sharath.prakash1992@gmail.com>
2020-04-27 03:36:20 +0800

    Updates

---
+++ precache-manifest.054ab8081c1b053de34af83db93d32e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3094d97de9852cc438e6721c88c2a0",
+    "url": "/ProShop/index.html"
+  },
+  {
+    "revision": "b9b403beffb424bf6be6",
+    "url": "/ProShop/static/css/main.afbe58c2.chunk.css"
+  },
+  {
+    "revision": "4c65c9a4c14fb82a0750",
+    "url": "/ProShop/static/js/2.1a170f0d.chunk.js"
+  },
+  {
+    "revision": "b9b403beffb424bf6be6",
+    "url": "/ProShop/static/js/main.938e8a0b.chunk.js"
+  },
+  {
+    "revision": "80e49db873f051177a03",
+    "url": "/ProShop/static/js/runtime-main.ea9ebdaa.js"
+  }
+]);
