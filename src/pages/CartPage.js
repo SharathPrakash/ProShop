6f0f80f842800a95a094cc89ca6ae1f627654431
SharathPrakash <sharath.prakash1992@gmail.com>
2021-10-18 22:46:37 +0530

    Update CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -6,9 +6,12 @@ const CartPage = () => {
 
     return (
         <div>
-            <div className="gridCss">
-                <CartList />
-            </div>
+//             <div className="gridCss">
+//                 <CartList />
+//             </div>
+        
+          <iframe src="https://www.figma.com/proto/NdfAOpVVg9GyB3LTeTYDPs/Wedding-Invitation-App-New-with-animation?node-id=236%3A519&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=236%3A519
+" title="wedding"></iframe>
 
             <CartFooter />
         </div>
