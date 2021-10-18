4e5f6ffa133d1f814a46c9e67a7552f56b685840
SharathPrakash <sharath.prakash1992@gmail.com>
2021-10-18 22:43:00 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,11 @@ import ProductsList from "../modules/product/components/ProductList";
 
 const HomePage = () => {
     return (
-        <ProductsList />
+        <div>
+        <iframe src="https://www.figma.com/proto/NdfAOpVVg9GyB3LTeTYDPs/Wedding-Invitation-App-New-with-animation?node-id=236%3A519&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=236%3A519
+" title="wedding"></iframe>
+//        <ProductsList />
+        </div>
     )
 };
 
